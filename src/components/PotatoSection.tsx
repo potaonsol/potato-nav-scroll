3fc19902b70f228f0732a2b7cfbc56300b988f17
potaonsol <potaonsol@gmail.com>
2025-08-06 20:21:44 +1000

    Update potato character gallery with equal sizing and beige frames

--- src/components/PotatoSection.tsx
+++ src/components/PotatoSection.tsx
@@ -27,46 +27,58 @@ const PotatoSection = () => {
           {/* Potato Characters Gallery */}
           <div className="grid grid-cols-3 gap-6 max-w-2xl mx-auto">
             <div className="flex justify-center">
-              <img 
-                src="/lovable-uploads/1b1cc06f-f17a-4ea1-8790-22ba74a27e2b.png" 
-                alt="Potato Tarot" 
-                className="h-24 md:h-32 w-auto object-contain"
-              />
+              <div className="bg-potato-beige p-4 rounded-2xl border-2 border-potato-brown/20 shadow-lg">
+                <img 
+                  src="/lovable-uploads/1b1cc06f-f17a-4ea1-8790-22ba74a27e2b.png" 
+                  alt="Potato Tarot" 
+                  className="h-48 md:h-64 w-48 md:w-64 object-contain"
+                />
+              </div>
             </div>
             <div className="flex justify-center">
-              <img 
-                src="/lovable-uploads/775d2571-c10e-473c-9b29-19b8ad000cee.png" 
-                alt="Happy Potato" 
-                className="h-24 md:h-32 w-auto object-contain"
-              />
+              <div className="bg-potato-beige p-4 rounded-2xl border-2 border-potato-brown/20 shadow-lg">
+                <img 
+                  src="/lovable-uploads/775d2571-c10e-473c-9b29-19b8ad000cee.png" 
+                  alt="Happy Potato" 
+                  className="h-48 md:h-64 w-48 md:w-64 object-contain"
+                />
+              </div>
             </div>
             <div className="flex justify-center">
-              <img 
-                src="/lovable-uploads/ef2179c3-752d-42da-97df-a374a52cccf0.png" 
-                alt="Love Potato" 
-                className="h-24 md:h-32 w-auto object-contain"
-              />
+              <div className="bg-potato-beige p-4 rounded-2xl border-2 border-potato-brown/20 shadow-lg">
+                <img 
+                  src="/lovable-uploads/ef2179c3-752d-42da-97df-a374a52cccf0.png" 
+                  alt="Love Potato" 
+                  className="h-48 md:h-64 w-48 md:w-64 object-contain"
+                />
+              </div>
             </div>
             <div className="flex justify-center">
-              <img 
-                src="/lovable-uploads/1fa31d05-98f1-455b-ad6d-8f6eb892b410.png" 
-                alt="Cool Potato" 
-                className="h-24 md:h-32 w-auto object-contain"
-              />
+              <div className="bg-potato-beige p-4 rounded-2xl border-2 border-potato-brown/20 shadow-lg">
+                <img 
+                  src="/lovable-uploads/1fa31d05-98f1-455b-ad6d-8f6eb892b410.png" 
+                  alt="Cool Potato" 
+                  className="h-48 md:h-64 w-48 md:w-64 object-contain"
+                />
+              </div>
             </div>
             <div className="flex justify-center">
-              <img 
-                src="/lovable-uploads/2f0bb0f1-1452-4f19-9b2e-fb465679389d.png" 
-                alt="Potato and Ketchup" 
-                className="h-24 md:h-32 w-auto object-contain"
-              />
+              <div className="bg-potato-beige p-4 rounded-2xl border-2 border-potato-brown/20 shadow-lg">
+                <img 
+                  src="/lovable-uploads/2f0bb0f1-1452-4f19-9b2e-fb465679389d.png" 
+                  alt="Potato and Ketchup" 
+                  className="h-48 md:h-64 w-48 md:w-64 object-contain"
+                />
+              </div>
             </div>
             <div className="flex justify-center">
-              <img 
-                src="/lovable-uploads/ccd6753a-1ec5-409e-bbb3-ad0ef3876bda.png" 
-                alt="Sleepy Potato" 
-                className="h-24 md:h-32 w-auto object-contain"
-              />
+              <div className="bg-potato-beige p-4 rounded-2xl border-2 border-potato-brown/20 shadow-lg">
+                <img 
+                  src="/lovable-uploads/ccd6753a-1ec5-409e-bbb3-ad0ef3876bda.png" 
+                  alt="Sleepy Potato" 
+                  className="h-48 md:h-64 w-48 md:w-64 object-contain"
+                />
+              </div>
             </div>
           </div>
         </div>
